321f023b5dbb23388d3e5ef62e8d51bf042ca98a
ranj-13 <ranjujvd@yahoo.com>
2025-02-03 17:53:19 +0530

    Update index.js
    adding index details

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const { anonymizeData, shuffleData } = require('./anonymizer');
+
+const inputFilePath = 'data/input.csv';
+const outputFilePath = 'data/output.csv';
+
+const columnsToAnonymize = {
+  name: 'mask',
+  email: 'mask',
+  phone: 'mask'
+};
+
+const anonymizedData = [];
+
+fs.createReadStream(inputFilePath)
+  .pipe(csv())
+  .on('data', (row) => {
+    anonymizedData.push(anonymizeData(row, columnsToAnonymize));
+  })
+  .on('end', () => {
+    const shuffledData = shuffleData(anonymizedData);
+    fs.writeFileSync(outputFilePath, JSON.stringify(shuffledData, null, 2));
+    console.log('Data anonymized and saved to', outputFilePath);
+  });
 
