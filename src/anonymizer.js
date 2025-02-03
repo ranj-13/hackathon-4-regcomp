52e6e4a6ab40721397e1a485e61f63ab60733f09
ranj-13 <ranjujvd@yahoo.com>
2025-02-03 17:54:33 +0530

    Update anonymizer.js
    adding code

--- src/anonymizer.js
+++ src/anonymizer.js
@@ -1,1 +1,21 @@
+const faker = require('faker');
 
+function anonymizeData(row, columnsToAnonymize) {
+  const anonymizedRow = { ...row };
+  for (const column in columnsToAnonymize) {
+    if (columnsToAnonymize[column] === 'mask') {
+      anonymizedRow[column] = faker.fake("{{name.firstName}} {{name.lastName}}");
+    }
+  }
+  return anonymizedRow;
+}
+
+function shuffleData(data) {
+  for (let i = data.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [data[i], data[j]] = [data[j], data[i]];
+  }
+  return data;
+}
+
+module.exports = { anonymizeData, shuffleData };
